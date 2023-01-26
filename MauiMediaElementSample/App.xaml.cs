641ca3ff0b27c8ae7142269fcd73e4992752910e
Gerald Versluis <gerald.versluis@microsoft.com>
2023-01-26 14:37:38 +0100

    Initial commit

---
+++ MauiMediaElementSample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiMediaElementSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MauiMediaElementSample
+ Sample code to demonstrate how to get started with MediaElement in your .NET MAUI application
